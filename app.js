4b662bd0d7442014ae0f58ea0c53060cda75d593
huishanChiu <jolley0901@gmail.com>
2021-08-17 17:09:07 +0800

    新增cookie部分

--- app.js
+++ app.js
@@ -1,36 +1,33 @@
-const express = require('express');
+const express = require("express");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const dataRoutes = require("./routes/getData");
+const nameRoutes = require("./routes/myName");
 const app = express();
+app.set("view engine", "pug");
 
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-
-
-
-
+app.use("/myName", nameRoutes);
 app.use("/getData", dataRoutes);
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + "/public"));
 
-app.get('/', function (req, res) {
-  res.send('<h3>Hello 你好嗎～衷心感謝</h3>,num');
+app.get("/", function (req, res) {
+  res.send("<h3>Hello 你好嗎～衷心感謝</h3>");
 });
 
-
-
 // app.get('/sum', function (req, res) {
-//     res.render('sum', { number: req.cookies.number });  
-    
+//     res.render('sum', { number: req.cookies.number });
+
 // });
 // app.post('/sum', function (req, res) {
 //     res.cookie("number", req.body.number);
 //     console.dir(req.body.number);
-//     res.render('sum', { number: req.body.number });  
-    
+//     res.render('sum', { number: req.body.number });
+
 // });
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+  console.log("Example app listening on port 3000!");
 });
--- routes/myName.js
+++ routes/myName.js
@@ -1,2 +1,14 @@
 const express = require("express");
 const router = express.Router();
+
+router.get("/", function (req, res) {
+  res.render("myName");
+});
+
+router.post("/", function (req, res) {
+  res.cookie("username", req.body.username);
+  console.dir(req.body.username);
+  res.redirect("/myName");
+});
+
+module.exports = router;
